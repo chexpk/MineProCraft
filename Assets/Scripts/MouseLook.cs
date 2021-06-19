957c438805b1e61b283e9f050bc525c62a503e70
chexPK <chexpk@gmail.com>
2021-06-19 14:35:13 +0300

    Correct moves

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float sensitivityHor = 9.0f;
-    public float sensitivityVert = 9.0f;
+    public float sensitivity = 9.0f;
     public float minimumVert = -45.0f;
     public float maximumVert = 45.0f;
     private float _rotationX = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //вращается все прикрепленное к Player
-        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        // вращается все прикрепленное к Player
+        float deltaX = Input.GetAxis("Mouse Y") * sensitivity;
+        _rotationX -= deltaX;
         _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
-        float delta = Input.GetAxis("Mouse X") * sensitivityHor;
-        float rotationY = transform.localEulerAngles.y + delta;
+
+        float deltaY = Input.GetAxis("Mouse X") * sensitivity;
+        float rotationY = transform.localEulerAngles.y + deltaY;
+
         transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
     }
 }
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed = 6.0f;
+    public bool isGrounded;
+    public float speed = 6.0F;
+    public float jumpSpeed = 2.0F;
+    public float gravity = 9.8F;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private Vector3 playerVelocity = Vector3.zero;
+    private CharacterController controller;
+
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        // For debug
+        isGrounded = controller.isGrounded;
+
+        // Stop move
+        playerVelocity.x = 0f;
+        playerVelocity.z = 0f;
+
+        // Move
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        move = transform.TransformDirection(move);
+        playerVelocity += move;
+
+        // Jump
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            playerVelocity.y = jumpSpeed;
+        }
+
+        // Приземление
+        if (controller.isGrounded && playerVelocity.y < 0f)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // Gravity
+        playerVelocity.y -= gravity * Time.deltaTime;
+
+        // Apply move
+        controller.Move(playerVelocity);
     }
 }
